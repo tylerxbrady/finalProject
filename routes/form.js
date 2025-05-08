c9cea0a01fc196ae180c10dce4978ab79940a6b1
Tyler Brady <tylerbrady@Tylers-MacBook-Pro.local>
2025-05-08 15:52:28 -0400

    seeing if this makes it so it can get to form page

--- routes/form.js
+++ routes/form.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 
-module.exports = function(port) {
+module.exports = function(portNumber) {
 
     const router = express.Router();
 
     router.get("/", (request, response) => { 
-        response.render("form", {port: port});
+        response.render("form", {port: portNumber});
        });
       
     return router;
